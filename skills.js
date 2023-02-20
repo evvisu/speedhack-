c607c2bce4721a0df7b0cc8c63162985f239e242
evvisu <spayder2211@gmail.com>
2023-02-20 22:40:44 +0300

    fix some shit

--- skills.js
+++ skills.js
@@ -1,40 +1,40 @@
 // valkyrie
 module.exports.glaiver = [
-	//{ "id": 1, "delay": 0, "speed": 50 }, // autoattack
+	{ "id": 1, "delay": 0, "speed": 50 }, // autoattack
 	{ "id": 3, "delay": 0, "speed": 25 }, // glaivestrike
 	{ "id": 5, "delay": 0, "speed": 25, "hasAbn": 10155130 }, // mael (during godsfall?)
 	{ "id": 5, "delay": 0, "speed": 25 }, // mael
 	{ "id": 6, "delay": 0, "speed": 25 }, // leaping
-	//{ "id": 7, "delay": 0, "speed": 0 }, // spinning
-	//{ "id": 8, "delay": 0, "speed": 20 }, // titansbane
+	{ "id": 7, "delay": 0, "speed": 0 }, // spinning
+	{ "id": 8, "delay": 0, "speed": 20 }, // titansbane
 	{ "id": 9, "delay": 0, "speed": 25 }, // groundbash
-	//{ "id": 10, "delay": 0, "speed": 25 }, // dreamslash
+	{ "id": 10, "delay": 0, "speed": 25 }, // dreamslash
 	{ "id": 11, "subId": 30, "delay": 0, "speed": 25 }, // sc 1 hit
 	{ "id": 11, "subId": 31, "delay": 0, "speed": 25 }, // sc 2 hit
 	{ "id": 11, "subId": 31, "delay": 0, "speed": 25, "hasAbn": 10155130 }, // sc 2nd hit (ragnarok)
-	//{ "id": 12, "delay": 0, "speed": 40 }, // ragnarok
+	{ "id": 12, "delay": 0, "speed": 40 }, // ragnarok
 	{ "id": 13, "delay": 0, "speed": 25 }, // bloodflower
-	//{ "id": 14, "unlock": true } // evade
-	//{ "id": 16, "delay": 560, "speed": 0, "unlock": true }, // runeburst
+	{ "id": 14, "unlock": true } // evade
+	{ "id": 16, "delay": 560, "speed": 0, "unlock": true }, // runeburst
 	//{ "id": 17, "delay": 0, "speed": 80, "unlock": true }, // balders
-	//{ "id": 19, "delay": 0, "speed": 20, "unlock": true }, // reclamation
+	{ "id": 19, "delay": 0, "speed": 20, "unlock": true }, // reclamation
 	{ "id": 23, "delay": 0, "speed": 25 }, // gungnir
-	//{ "id": 24, "subId": 0, "delay": 1060, "speed": 20 }, // tw1
-	//{ "id": 24, "subId": 2, "delay": 1060, "speed": 20 }, // tw2
-	//{ "id": 24, "subId": 3, "delay": 1260, "speed": 20 }, // tw3
-	//{ "id": 24, "subId": 4, "delay": 1560, "speed": 50 }, // tw4
-	//{ "id": 25, "subId": 0, "delay": 0, "speed": 400 }, // godsfall (cast)
-	//{ "id": 25, "subId": 2, "delay": 0, "speed": 170 }, // godsfall (flight)
+	{ "id": 24, "subId": 0, "delay": 1060, "speed": 20 }, // tw1
+	{ "id": 24, "subId": 2, "delay": 1060, "speed": 20 }, // tw2
+	{ "id": 24, "subId": 3, "delay": 1260, "speed": 20 }, // tw3
+	{ "id": 24, "subId": 4, "delay": 1560, "speed": 50 }, // tw4
+	{ "id": 25, "subId": 0, "delay": 0, "speed": 100 }, // godsfall (cast)
+	{ "id": 25, "subId": 2, "delay": 0, "speed": 78 }, // godsfall (flight)
 ];
 // ninja
 module.exports.assassin = [
-	//{ "id": 1, "delay": 0, "speed": 30, "unlock": false }, // autoattack
-	//{ "id": 2, "delay": 0, "speed": 7, "unlock": true }, // shadowjump
+	{ "id": 1, "delay": 0, "speed": 30, "unlock": false }, // autoattack
+	{ "id": 2, "delay": 0, "speed": 7, "unlock": false }, // shadowjump
 	//{ "id": 3, "delay": 800, "speed": 10 }, // lotw
-	//{ "id": 5, "delay": 0, "speed": 0 }, // impactbomb
+	{ "id": 5, "delay": 0, "speed": 10 }, // impactbomb
 	//{ "id": 6, "unlock": true } // 1k cuts
-	{ "id": 12, "delay": 0, "speed": 25 }, // skyfall
-	//{ "id": 13, "delay": 1000, "speed": 40 }, // cos
+	{ "id": 12, "delay": 0, "speed": 20 }, // skyfall
+	//{ "id": 13, "delay": 1000, "speed": 40 }, // cos (BROOOOOOOHHWWWWWTTFFFFF)
 	{ "id": 14, "delay": 0, "speed": 25 }, // dc
 	//{ "id": 19, "delay": 0, "speed": 20 }, // chakra thrust 
 	//{ "id": 21, "subId": 50, "delay": 1100, "speed": 0, "fixedDelay": false}, // bs 3 out of ult
@@ -45,37 +45,37 @@ module.exports.assassin = [
 ];
 // sorcerer
 module.exports.sorcerer = [
-	{ "id": 2, "delay": 0, "speed": 40 }, // FrostSphere
-	{ "id": 4, "delay": 0, "speed": 10 }, // ArcanePulse
+	{ "id": 2, "delay": 0, "speed": 110 }, // FrostSphere
+	{ "id": 4, "delay": 0, "speed": 15 }, // ArcanePulse
 	//{ "id": 8, "delay": 0, "speed": 40 }, // FlamePillar
-	{ "id": 11, "delay": 0, "speed": 40 }, // LightningStrike
-	{ "id": 12, "delay": 0, "speed": 100 }, // VoidPulse
+	{ "id": 11, "delay": 0, "speed": 90 }, // LightningStrike
+	{ "id": 12, "delay": 0, "speed": 140 }, // VoidPulse
 	{ "id": 17, "delay": 0, "speed": 40 }, // PainfulTrap
 	{ "id": 20, "delay": 0, "speed": 40 }, // FlamingBarrage
 	{ "id": 23, "delay": 0, "speed": 40 }, // ManaVolley
-	{ "id": 27, "delay": 0, "speed": 90 }, // HailStorm
-	{ "id": 30, "delay": 0, "speed": 10 }, // Nova
-	{ "id": 34, "delay": 0, "speed": 40 }, // ManaBoost
-	//{ "id": 35, "delay": 0, "speed": 40 }, // IceLances
-	{ "id": 36, "subId": 200, "delay": 0, "speed": 10 }, // PrimeFlame
-	{ "id": 36, "subId": 300, "delay": 0, "speed": 10 }, // ArcaneStorm
-	{ "id": 36, "subId": 400, "delay": 0, "speed": 10 }, // Iceberg
+	{ "id": 27, "delay": 0, "speed": 140 }, // HailStorm
+	{ "id": 30, "delay": 0, "speed": 5 }, // Nova
+	{ "id": 34, "delay": 0, "speed": 50 }, // ManaBoost
+	//{ "id": 35, "delay": 0, "speed": 40 }, // IceLances (didntworkxd)
+	//{ "id": 36, "subId": 200, "delay": 0, "speed": 10 }, // PrimeFlame
+	//{ "id": 36, "subId": 300, "delay": 0, "speed": 10 }, // ArcaneStorm
+	//{ "id": 36, "subId": 400, "delay": 0, "speed": 10 }, // Iceberg
 	{ "id": 36, "subId": 600, "delay": 0, "speed": 0 }, // ElementalFusion
-	//{ "id": 39, "delay": 0, "speed": 40 }, // Implosion 
+	//{ "id": 39, "delay": 0, "speed": 40 }, // Implosion (dont touch it desync sometimes)
 	{ "id": 61, "delay": 10, "speed": 100 }, // MeteorStrike
 ];
 // lancer  
 module.exports.lancer = [
 	//{ "id": 1, "delay": 0, "speed": 20 }, // autoattack
-	{ "id": 3, "delay": 0, "speed": 25 }, // onslaught
+	{ "id": 3, "delay": 0, "speed": 15 }, // onslaught
 	//{ "id": 4, "delay": 0, "speed": 25 }, // aggro shout
-	{ "id": 5, "delay": 0, "speed": 25 }, // shield bash
+	{ "id": 5, "delay": 0, "speed": 15 }, // shield bash
 	//{ "id": 12, "delay": 0, "speed": 10 }, // enrage
-	{ "id": 13, "delay": 0, "speed": 25 }, // spring
-	{ "id": 18, "delay": 0, "speed": 25 }, // barrage
-	{ "id": 21, "delay": 0, "speed": 25 }, // lockdown
-	{ "id": 25, "delay": 0, "speed": 25 }, // wallop
-	{ "id": 28, "delay": 0, "speed": 25 }, // leap
+	{ "id": 13, "delay": 0, "speed": 20 }, // spring
+	{ "id": 18, "delay": 0, "speed": 20 }, // barrage
+	{ "id": 21, "delay": 0, "speed": 20 }, // lockdown
+	{ "id": 25, "delay": 0, "speed": 20 }, // wallop
+	{ "id": 28, "delay": 0, "speed": 20 }, // leap
 ];
 // slayer
 module.exports.slayer = [
@@ -99,7 +99,7 @@ module.exports.soulless = [
     { "id": 3, "delay": 0, "speed": 25 }, // double shear
     { "id": 4, "delay": 0, "speed": 25 }, // sundering
     { "id": 5, "delay": 0, "speed": 25 }, // grim 
-	//{ "id": 6, "delay": 0, "speed": 25 }, // death spiral
+	{ "id": 6, "delay": 0, "speed": 20 }, // death spiral
 	{ "id": 8, "delay": 0, "speed": 25 }, //whipsaw
 	{ "id": 9, "delay": 0, "speed": 25 }, // smite 
 	{ "id": 10, "delay": 0, "speed": 25 }, // pendulum
